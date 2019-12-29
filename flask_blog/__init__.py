478e998b58a60fec57cee04a2772ff8abdd83a8f
bihire <muhireboris@yahoo.fr>
2019-12-29 15:41:15 +0200

    authentication user

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '93d76c3d2188e4980071b1198473d790'
@@ -8,5 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flask_blog import routes
