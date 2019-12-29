1e6529936e9a3c4d1122f9f8c71517cc84332261
bihire <muhireboris@yahoo.fr>
2019-12-29 16:11:18 +0200

    add default image

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -77,4 +77,5 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    return render_template('account.html', title='My Account')
+    image_file = url_for('static', filename='images/' + current_user.image_file)
+    return render_template('account.html', title='My Account', image_file=image_file)
